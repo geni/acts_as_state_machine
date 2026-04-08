457f0653e364fa6c22bede1f78e31d5a7528761c
Scott Steadman <scott.steadman@geni.com>
2026-04-08 10:20:21 -0700

    remove git gc call from test_my_app.sh
      This will be handled by jenkins.

--- test_my_app.sh
+++ test_my_app.sh
@@ -4,8 +4,6 @@ unset RBENV_VERSION
 export RBENV_ROOT=/opt/rbenv
 export PATH=${RBENV_ROOT}/shims:${PATH}:${RBENV_ROOT}/bin
 
-git gc
-
 #bundle config --local build.pg "--with-pg-config=/usr/pgsql-15/bin/pg_config"
 bundle config --local clean true
 bundle config --local path vendor/bundle
